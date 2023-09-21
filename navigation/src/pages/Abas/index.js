ea86fbef9970d19eb9d0b9a4803d4e617a2bd95b
VITÓRIO AUGUSTO DA SILVA DE SOUZA <vitorio_souza@estudante.sesisenai.org.br>
2023-09-21 14:32:41 -0300

    Criação do Abas

--- navigation/src/pages/Abas/index.js
+++ navigation/src/pages/Abas/index.js
@@ -2,25 +2,18 @@ import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-export default function Home() {
+export default function Home(){
     const navigation = useNavigation();
-    return (
+    return(
         <View style={styles.container}>
-            <Text>Tela Home</Text>
-            <Button
-                title="Ir pra Sobre"
-                onPress={() => navigation.navigate('Sobre')}
-            />
-            <Button
-                title="Navegar pra Abas"
-                onPress={() => navigation.navigate('Abas')}
-            />
+            <Text>Tela Abas</Text>
+            <Text>Não foi eu que escolhi esse nome!</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    container:{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
